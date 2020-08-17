3fb5f4b213ea75a6807916773fd8309d481cd432
muthukrishk <mkrishnamurthy1@woolworths.com.au>
2020-08-17 10:13:12 +1000

    Minor fixes for error message misalignment

--- src/_components/ErrorMessage/style.js
+++ src/_components/ErrorMessage/style.js
@@ -2,7 +2,7 @@ import styled from 'styled-components'
 
 export const Message = styled.div`
   position: absolute;
-  bottom: 20px;
+  bottom: 10px;
   font-size: 12px;
   left: 10px;
   color: red;
